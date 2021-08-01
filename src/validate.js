f25e54d5b8b264c3d1b7a16948fea9d9055d1cb2
Lilit Yepremyan <lilit.epremyan.96@gmail.com>
2021-08-01 19:08:59 +0400

    fix:change email regexp

--- src/validate.js
+++ src/validate.js
@@ -1,41 +1,37 @@
 const validate = (values) => {
-   const errors = {};
-   if(!values.firstName){
-   errors.firstName = "Last name is required"
-   }
-   else if(!/[!0-9@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/.test(values.firstName)){
-   errors.name ="Invalid  last name"
-   }
-    
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Last name is required';
+  } else if (!/[!0-9@#$%^&*()_+\-=[\]{};':"\\|,<>/?]/.test(values.firstName)) {
+    errors.name = 'Invalid  last name';
+  }
 
-    if(!values.surName){
-        errors.surName = "Surname is required"
-    }
-    else if(!/^[a-zA-Z ]+$/.test(values.surName)){
-    errors.surName ="Invalid Surename"
-    }
+  if (!values.surName) {
+    errors.surName = 'Surname is required';
+  } else if (!/^[a-zA-Z ]+$/.test(values.surName)) {
+    errors.surName = 'Invalid Surename';
+  }
 
+  if (!values.email) {
+    errors.email = 'Email address is required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
 
-    if(!values.email){
-    errors.email= "Email address is required"
-    }
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test (values.email)){
-    errors.email = "Invalid email address"}
-
-
-    if(!values.password){
-        errors.password = "Password is required"
-    }
-    else if (!/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(values.password)){
-     errors.password = " Password  must has min 8 letter, with at least a symbol, upper and lower case letters and a number"
-    }
-return errors
-}
+  if (!values.password) {
+    errors.password = 'Password is required';
+  } else if (
+    !/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(
+      values.password
+    )
+  ) {
+    errors.password =
+      ' Password  must has min 8 letter, with at least a symbol, upper and lower case letters and a number';
+  }
+  return errors;
+};
 
 export default validate;
-
-
-
 
 // const validate = (values) => {
 //     const errors = {};
@@ -46,7 +42,7 @@ export default validate;
 //     } else {
 //       errors.firstName = "Nice first name ";
 //     }
-  
+
 //     if (!values.lastName) {
 //       errors.lastName = "Last name is required";
 //     } else if (values.lastName.length < 1) {
@@ -54,13 +50,13 @@ export default validate;
 //     } else {
 //       errors.lastName = "Awesome last name ";
 //     }
-  
+
 //     if (!values.email) {
 //       errors.email = "Email address is required";
 //     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
 //       errors.email = "Invalid email address";
 //     }
-  
+
 //     if (!values.password) {
 //       errors.password = "Password is required";
 //     } else if (values.password.length <= 6) {
@@ -68,9 +64,8 @@ export default validate;
 //     } else {
 //       errors.password = "Password strength is ok ";
 //     }
- 
+
 //     return errors;
 //   };
-  
-// export default validate;
 
+// export default validate;
